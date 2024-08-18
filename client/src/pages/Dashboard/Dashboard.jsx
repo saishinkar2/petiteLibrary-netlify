d6f3f30a56607a62088f6cd67ee71776a5628246
saishinkar2 <141svs@gmail.com>
2024-08-18 21:01:06 +0530

    Changed Name

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
 
@@ -7,33 +7,33 @@ export const Dashboard = () => {
   return (
     <div className="overflow-hidden">
       <div className="fixed h-14 top-0 bg-[#1B4242] w-full flex items-center justify-between">
-        <div className="p-2">PETITE LIBRARY</div>
+        <div className="p-2">PETIT LIBRARY</div>
         <div className="flex gap-8">
-          <NavLink 
+          <NavLink
             to={"books"}
             className={({ isActive }) => isActive ? "border-b-2" : ""}
           >
             BOOKS
           </NavLink>
-          <NavLink 
+          <NavLink
             to={"members"}
             className={({ isActive }) => isActive ? "border-b-2" : ""}
           >
             MEMBERS
           </NavLink>
-          <NavLink 
+          <NavLink
             to={"issues"}
             className={({ isActive }) => isActive ? "border-b-2" : ""}
           >
             ISSUE
           </NavLink>
         </div>
-        <div/>
+        <div />
 
       </div>
       <div className="mt-14 h-[calc(100vh-8rem)]">
-        <Outlet/>
+        <Outlet />
       </div>
     </div>
-  )
-}
+  );
+};
